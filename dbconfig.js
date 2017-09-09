999c1018ceffb7aa0bd0b18b801669efc05abf53
Patrick Brown Jr <pjbrn26@gmail.com>
2017-09-08 19:02:12 -0500

    Posting to Database

--- dbconfig.js
+++ dbconfig.js
@@ -1,13 +1,28 @@
-var mongoose = require("mongoose");
+const mongoURI =
+  "mongodb://pj26:awash1991@ds157320.mlab.com:57320/basketballtraining";
+console.log(mongoURI);
+const mongoose = require("mongoose");
+mongoose.Promise = require("bluebird");
 
-mongoURI = "mongodb://localhost/shortlydb";
-mongoose.connect(mongoURI);
+mongoose
+  .connect(mongoURI, { useMongoClient: true })
+  .then(({ db: { databaseName } }) =>
+    console.log(`Connected to ${databaseName}`))
+  .catch(err => console.error(err));
 
-// Run in seperate terminal window using 'mongod'
-var db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function() {
-  console.log("Mongodb connection open");
+const sessionSchema = mongoose.Schema({
+  name: String,
+  type: String,
+  duration: String
 });
+
+const Session = mongoose.model("Session", sessionSchema);
+
+const insertSession = session =>
+  new Session(session).save((err, newSession) =>
+    console.log(err || newSession)
+  );
+
+
 
 module.exports = db;
--- server.js
+++ server.js
@@ -1,7 +1,29 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+//const db = require('./dbconfig.js')
 
+//const mongoURI = process.env.MONGO_URI;
+const mongoURI = "mongodb://pj26:awash1991@ds157320.mlab.com:57320/basketballtraining";
+const mongoose = require("mongoose");
+mongoose.Promise = require('bluebird');
+
+
+mongoose.connect(mongoURI, {useMongoClient: true})
+  .then(({db: {databaseName}}) => console.log(`Connected to ${databaseName}`))
+  .catch((err => console.error(err)));
+
+const sessionSchema = mongoose.Schema({
+  name: String,
+  type: String,
+  duration: String
+});
+
+const Session = mongoose.model("Session", sessionSchema);
+
+const insertSession = session => 
+  (new Session(session)).save( (err, newSession) => 
+    console.log(err || newSession));
 
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -10,12 +32,12 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req,res) => {
-  console.log(req.body);
+  console.log("Post Worked");
+  insertSession({ name: req.body.name, type: req.body.type, duration: req.body.duration });
 })
 
-
-app.listen(8080, () => {
-  console.log('listening on 8080');
+app.listen('8080', () => {
+  console.log(`Listening on 8080`);
 })
 
 console.log("Node working")
